ac7a92e50e8128c2d294062d204d78c747223a47
Adam Shepley <ajshepley@gmail.com>
2020-01-09 23:38:13 -0500

    Added final and this qualifiers.

--- src/main/java/com/github/ajshepley/MainSketch.java
+++ src/main/java/com/github/ajshepley/MainSketch.java
@@ -4,15 +4,15 @@ import processing.core.PApplet;
 
 public class MainSketch extends PApplet {
   public void settings(){
-    size(200, 200);
+    this.size(200, 200);
   }
 
   public void draw(){
-    background(0);
-    ellipse(mouseX, mouseY, 20, 20);
+    this.background(0);
+    this.ellipse(this.mouseX, this.mouseY, 20, 20);
   }
 
-  public static void main(String[] args){
+  public static void main(final String[] args){
 //    PApplet.main("MainSketch");
     PApplet.main(new String[] { "--present", MainSketch.class.getCanonicalName() });
   }
